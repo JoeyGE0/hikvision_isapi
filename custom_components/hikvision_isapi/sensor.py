ed849b411bb58ca82b0f382f55f2d9ec1b4e3951
Josiah <115068320+JoeyGE0@users.noreply.github.com>
2025-11-22 14:03:29 +1300

    Update sensor.py

--- custom_components/hikvision_isapi/sensor.py
+++ custom_components/hikvision_isapi/sensor.py
@@ -1,68 +1,30 @@
-import logging
-import requests
-import xml.etree.ElementTree as ET
+from __future__ import annotations
 
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
-
 
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
-    async_add_entities: AddEntitiesCallback
+    async_add_entities,
 ):
-    config = hass.data[DOMAIN][entry.entry_id]
+    coordinator = hass.data[DOMAIN][entry.entry_id]
 
-    host = config[CONF_HOST]
-    username = config[CONF_USERNAME]
-    password = config[CONF_PASSWORD]
-
-    entities = [
-        HikvisionIRCutSensor(host, username, password),
-    ]
-
-    async_add_entities(entities, True)
+    async_add_entities([
+        HikvisionIRModeSensor(coordinator),
+    ])
 
 
-class HikvisionIRCutSensor(SensorEntity):
-    """IR Cut filter mode sensor"""
-
-    _attr_name = "Hikvision IR Cut Mode"
-    _attr_unique_id = "hikvision_ircut_mode"
-
-    def __init__(self, host, username, password):
-        self._host = host
-        self._username = username
-        self._password = password
-        self._state = None
+class HikvisionIRModeSensor(CoordinatorEntity, SensorEntity):
+    _attr_name = "Hikvision IR Mode"
+    _attr_unique_id = "hikvision_ir_mode"
 
     @property
-    def native_value(self):
-        return self._state
-
-    def update(self):
-        url = f"http://{self._host}/ISAPI/Image/channels/1/IrcutFilter"
-
-        try:
-            r = requests.get(
-                url,
-                auth=(self._username, self._password),
-                verify=False,
-                timeout=5
-            )
-            xml = ET.fromstring(r.text)
-
-            mode = xml.find(".//{http://www.hikvision.com/ver20/XMLSchema}IrcutFilterType")
-
-            self._state = mode.text.strip() if mode is not None else "unknown"
-
-        except Exception as e:
-            _LOGGER.error("Failed updating IR Cut sensor: %s", e)
-            self._state = "error"
+    def state(self):
+        root = self.coordinator.data
+        mode = root.findtext(".//IrcutFilterType")
+        return mode
