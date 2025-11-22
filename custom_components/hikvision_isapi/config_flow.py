4f27fb3e98c3b9312a250945138e3de1cd250993
Josiah <115068320+JoeyGE0@users.noreply.github.com>
2025-11-22 13:35:10 +1300

    Update config_flow.py

--- custom_components/hikvision_isapi/config_flow.py
+++ custom_components/hikvision_isapi/config_flow.py
@@ -1,34 +1,30 @@
-from homeassistant import config_entries
+"""Config flow for the Hikvision ISAPI integration."""
 import voluptuous as vol
+from homeassistant import config_entries
+from .const import DOMAIN  # define DOMAIN = "hikvision_isapi" in const.py
 
-from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD, CONF_VERIFY_SSL
-from .const import DOMAIN
+DATA_SCHEMA = vol.Schema({
+    vol.Required("host"): str,
+    vol.Required("username"): str,
+    vol.Required("password"): str,
+})
 
+class HikvisionISAPIConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Hikvision ISAPI."""
 
-class HikvisionIsapiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
+    STEP_USER = "user"
 
     async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
         errors = {}
-
         if user_input is not None:
-            await self.async_set_unique_id(user_input[CONF_HOST])
-            self._abort_if_unique_id_configured()
-
-            return self.async_create_entry(
-                title=f"Hikvision {user_input[CONF_HOST]}",
-                data=user_input,
-            )
-
-        data_schema = vol.Schema({
-            vol.Required(CONF_HOST): str,
-            vol.Required(CONF_USERNAME): str,
-            vol.Required(CONF_PASSWORD): str,
-            vol.Optional(CONF_VERIFY_SSL, default=False): bool,
-        })
+            # TODO: try connecting to camera/api to validate credentials
+            # if fails:
+            #   errors["base"] = "cannot_connect"
+            # else:
+            return self.async_create_entry(title=user_input["host"], data=user_input)
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=data_schema,
-            errors=errors,
+            step_id=self.STEP_USER, data_schema=DATA_SCHEMA, errors=errors
         )
