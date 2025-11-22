d3e1ec68b76b36291b352122f0a77919c850cf86
Josiah <115068320+JoeyGE0@users.noreply.github.com>
2025-11-22 14:29:02 +1300

    Update __init__.py

--- custom_components/hikvision_isapi/__init__.py
+++ custom_components/hikvision_isapi/__init__.py
@@ -12,12 +12,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    await hass.config_entries.async_forward_entry_setups(
+        entry, ["sensor", "select", "number"]
+    )
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    await hass.config_entries.async_unload_platforms(
+        entry, ["sensor", "select", "number"]
+    )
     hass.data[DOMAIN].pop(entry.entry_id, None)
     return True
