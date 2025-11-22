bd845b52de3e1bcba53dd46c57163ebb61bce673
Josiah <115068320+JoeyGE0@users.noreply.github.com>
2025-11-22 16:05:51 +1300

    Update config_flow.py

--- custom_components/hikvision_isapi/config_flow.py
+++ custom_components/hikvision_isapi/config_flow.py
@@ -1,22 +1,35 @@
 """Config flow for the Hikvision ISAPI integration."""
-import voluptuous as vol
-from homeassistant import config_entries
-from .const import DOMAIN, CONF_UPDATE_INTERVAL, DEFAULT_UPDATE_INTERVAL
+from __future__ import annotations
 
-DATA_SCHEMA = vol.Schema({
-    vol.Required("host"): str,
-    vol.Required("username"): str,
-    vol.Required("password"): str,
-    vol.Optional(CONF_UPDATE_INTERVAL, default=DEFAULT_UPDATE_INTERVAL): vol.All(
-        vol.Coerce(int), vol.Range(min=5, max=300)
-    ),
-})
+import voluptuous as vol
+
+from homeassistant import config_entries
+
+from .const import (
+    CONF_HOST,
+    CONF_PASSWORD,
+    CONF_UPDATE_INTERVAL,
+    CONF_USERNAME,
+    DEFAULT_UPDATE_INTERVAL,
+    DOMAIN,
+)
+
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_HOST): str,
+        vol.Required(CONF_USERNAME): str,
+        vol.Required(CONF_PASSWORD): str,
+        vol.Optional(CONF_UPDATE_INTERVAL, default=DEFAULT_UPDATE_INTERVAL): vol.All(
+            vol.Coerce(int), vol.Range(min=5, max=300)
+        ),
+    }
+)
+
 
 class HikvisionISAPIConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Hikvision ISAPI."""
 
     VERSION = 1
-    STEP_USER = "user"
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
@@ -26,8 +39,10 @@ class HikvisionISAPIConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             # if fails:
             #   errors["base"] = "cannot_connect"
             # else:
-            return self.async_create_entry(title=user_input["host"], data=user_input)
+            await self.async_set_unique_id(user_input[CONF_HOST])
+            self._abort_if_unique_id_configured()
+            return self.async_create_entry(title=user_input[CONF_HOST], data=user_input)
 
         return self.async_show_form(
-            step_id=self.STEP_USER, data_schema=DATA_SCHEMA, errors=errors
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
